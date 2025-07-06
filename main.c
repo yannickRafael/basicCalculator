f5715317c3b39de8f6b4e921f6cd691153bdeafa
yannickRafael <yannickrafael286@gmail.com>
2025-07-06 14:16:23 +0200

    Refactor main function signature and enhance window properties in main.c

--- main.c
+++ main.c
@@ -3,13 +3,15 @@
 #include <string.h>
 #include <stdbool.h>
 
-int main(){
+int main(int argc, char *argv[]) {
     GtkWidget *window;
     gtk_init(NULL, NULL);
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_window_set_title(GTK_WINDOW(window), "Basic Calculator");
     gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
+    gtk_window_set_resizable(GTK_WINDOW(window), false);
     
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     
