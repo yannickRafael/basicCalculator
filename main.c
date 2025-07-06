cb90cbcb6952302acb389e7dca14dc705151bc1c
yannickRafael <yannickrafael286@gmail.com>
2025-07-06 14:13:34 +0200

    Update .gitignore and implement basic GTK window in main.c

--- main.c
+++ main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <gtk/gtk.h>
+#include <string.h>
+#include <stdbool.h>
 
 int main(){
+    GtkWidget *window;
+    gtk_init(NULL, NULL);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Basic Calculator");
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
     
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    
+    gtk_widget_show_all(window);
+    gtk_main();
+
+    return 0;
 }
